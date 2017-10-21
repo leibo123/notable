5ee9ba59a7d71dafaaf67882c581581cabf9911e
Hitol <32802583+Hitol@users.noreply.github.com>
2017-10-21 18:11:26 -0400

    Add files via upload

---
+++ StraightBulletMovement.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StraightBulletMovement : MonoBehaviour {
+
+    public float speed = 1.0f;
+	// Update is called once per frame
+	void Update () {
+        transform.position += Vector3.right * speed * Time.deltaTime;
+    }
+}
