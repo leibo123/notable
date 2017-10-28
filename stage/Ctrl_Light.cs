448bdf81ca195f81f929d24669a7efc226625580
Leon Lei <leon_lei@brown.edu>
2017-10-28 15:36:02 -0400

    Add files via upload

---
+++ stage/Ctrl_Light.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ctrl_Light : MonoBehaviour {
+
+    // Use this for initialization
+    public float speed = 50f;
+    public Light lt;
+	void Start () {
+        lt = GetComponent<Light>();
+	}
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position += Vector3.left * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += Vector3.right * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += Vector3.up * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position += Vector3.down * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.Q))
+        {
+            lt.spotAngle-= Time.deltaTime * speed/2;
+        }
+        if (Input.GetKey(KeyCode.E))
+        {
+            lt.spotAngle += Time.deltaTime * speed/2;
+        }
+    }
+}
