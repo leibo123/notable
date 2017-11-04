e8b8adca4fc3b095f2c8291eaac0db295035c2bb
Hitol <32802583+Hitol@users.noreply.github.com>
2017-11-04 15:07:00 -0400

    Add files via upload

---
+++ bullet/Spawner.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public float health;
+    public GameObject bullet;
+    public float spawnTime = 3f;
+    //public Transform[] spawnPoints;
+    public Vector3 spawnValues;
+    // Use this for initialization
+    // yes, but why o.
+    // nice people writing extendable code lol.
+    // I'm steadily understanding how important good code practice is.
+    void Start()
+    {
+        InvokeRepeating("Spawn", spawnTime, spawnTime);
+    }
+
+    void Spawn()
+    {
+        if (health <= 0f)
+        {
+            return;
+        }
+       // int spawnPointIndex = Random.Range(0, spawnPoints.Length);
+
+        Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), Random.Range(-spawnValues.y, spawnValues.y), 1); 
+
+        Instantiate(bullet, spawnPosition + transform.TransformPoint(0,0,0) , gameObject.transform.rotation);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //	GameObject
+    }
+}
---
+++ bullet/WallCleanUp.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallCleanUp : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        //Check the provided Collider2D parameter other to see if it is tagged "PickUp", if it is...
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            //the object getting wrecked is the lil bullet
+            //yea, so can I not make it be self-referential lol, it already checks the object in the if right?
+            //the wall?
+            //not exactly, it's a collider
+            // Yea ooooo
+            GameObject.Destroy(other.gameObject);
+        }
+    }
+}
