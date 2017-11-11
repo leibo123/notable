1243cac3dea4d9313a1cbaec4accc3770fdb70a7
Leon Lei <leon_lei@brown.edu>
2017-11-11 16:02:11 -0500

    Update Ctrl_Light.cs

--- stage/Ctrl_Light.cs
+++ stage/Ctrl_Light.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,8 +7,10 @@ public class Ctrl_Light : MonoBehaviour {
     // Use this for initialization
     public float speed = 50f;
     public Light lt;
+    public CircleCollider2D cc;
 	void Start () {
         lt = GetComponent<Light>();
+        cc = GetComponent<CircleCollider2D>();
 	}
 	// Update is called once per frame
 	void Update () {
@@ -36,5 +38,18 @@ public class Ctrl_Light : MonoBehaviour {
         {
             lt.spotAngle += Time.deltaTime * speed/2;
         }
+        if (lt.spotAngle < 50)
+        {
+            cc.radius = lt.spotAngle / 5f * 2f;
+        }
+        else
+        {
+            //Range should be set to 800.
+            //Position at -50
+            cc.radius = -35.21286f + 2.000698f * lt.spotAngle - 0.02330947f * Mathf.Pow(lt.spotAngle, 2) + 0.0001180536f * Mathf.Pow(lt.spotAngle, 3);
+            //cc.radius = 46.9837f - 1.022956f * lt.spotAngle + 0.01061152f * Mathf.Pow(lt.spotAngle, 2);
+        }
+        //cc.radius = -35.21286f + 2.000698f * lt.spotAngle - 0.02330947f * Mathf.Pow(lt.spotAngle, 2) + 0.0001180536f * Mathf.Pow(lt.spotAngle, 3);
+      //  cc.radius = 156657704f + (13.76671f - 156657700f) / (1 + Mathf.Pow((lt.spotAngle / 7784.218f), 3.533164f));
     }
 }
