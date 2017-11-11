b4b9b0aeb7b6022b8f0646bfbedd0016c1c3fdc9
Leon Lei <leon_lei@brown.edu>
2017-11-11 16:01:52 -0500

    Update Ctrl.cs

--- stage/Ctrl.cs
+++ stage/Ctrl.cs
@@ -1,20 +1,38 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Ctrl : MonoBehaviour
 {
     public float speed = 1.5f;
+    private float temp;
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        temp = speed;
+        if (collision.tag.Equals("Spotlight"))
+        {
+            speed *= 2f;
+            GetComponent<SpriteRenderer>().color = new Color(1f, 1f, 1f);
+        }
+    }
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        speed = temp;
+        GetComponent<SpriteRenderer>().color = new Color(1f, 0.5f, 0.5f);
+    }
 
     void Update()
     {
         if (Input.GetKey(KeyCode.LeftArrow))
         {
             transform.position += Vector3.left * speed * Time.deltaTime;
+            GetComponent<SpriteRenderer>().flipX = false;
         }
         if (Input.GetKey(KeyCode.RightArrow))
         {
             transform.position += Vector3.right * speed * Time.deltaTime;
+            GetComponent<SpriteRenderer>().flipX = true;
         }
         if (Input.GetKey(KeyCode.UpArrow))
         {
